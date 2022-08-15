060b301f54916977b07dcaae3358f20132515375
Alex Sun <alexsun@mailbox.org>
2022-08-16 00:01:25 +0200

    Add Readme and theme base

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # emacs-colors-plain
+
+An extended and slightly modified port of
+[vim-colors-plain](https://github.com/andreypopp/vim-colors-plain) for Emacs.
---
+++ emacs-colors-plain.el
@@ -0,0 +1,60 @@
+;;; emacs-colors-plain.el -- Minimal theme for Emacs.
+
+;; Author: Alex Sun <alexsun82@icloud.com>
+;; URL: https://github.com/ranmaru22/emacs-colors-plain
+;; Version: 0.1
+
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(deftheme colors-plain
+  "Minimal theme for Emacs.")
+
+(let (())
+
+  (custom-theme-set-variables
+   'colors-plain
+   '(frame-background-mode (quote dark)))
+
+  (custom-theme-set-faces
+   'colors-plain)
+
+
+
+  )
+
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'colors-plain)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; indent-tabs-mode: nil
+;; End:
+
+;;; emacs-colors-plain.el ends here.
